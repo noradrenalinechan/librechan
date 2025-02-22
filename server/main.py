ad269e19ecc3922b4636b23288ff54a81a59aa7a
noradrenalinechan <zxcvbnww221@gmail.com>
2025-02-22 23:30:50 +0200

    first commit

--- server/main.py
+++ server/main.py
@@ -22,4 +22,4 @@ app.include_router(comment.router, prefix="/api")
 app.include_router(admin_router.router, prefix="/api")
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
